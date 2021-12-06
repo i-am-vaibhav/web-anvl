5a99808b8430f6fecc05548b16b498ae3956711a
Vaibhav_Gupta_Olive <vaibhav@olivecrypto.com>
2021-12-06 15:57:09 +0530

    Added ratings also

--- src/main/resources/static/js/product.js
+++ src/main/resources/static/js/product.js
@@ -14,6 +14,17 @@ $(document).ready(function() {
 		}
 	});
 
+	/*$(".more").mouseout(function() {
+		let text = $(this).text();
+		if (text == "less") {
+			//$(this).text("less");
+			//$(this).parent().prev().toggleClass("text-truncate");
+		//} else {
+			$(this).text("more");
+			$(this).parent().prev().addClass("text-truncate");
+		}
+	});*/
+
 });
 
 //$.getJSON("https://jsonip.com?callback=webanvl", function(data) {
