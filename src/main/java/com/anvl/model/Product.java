280b4fa811b64f9f30e2d4a3aa0907651d48018d
Vaibhav_Gupta_Olive <vaibhav@olivecrypto.com>
2021-12-06 11:51:37 +0530

    Added guest product page

--- src/main/java/com/anvl/model/Product.java
+++ src/main/java/com/anvl/model/Product.java
@@ -6,8 +6,6 @@ package com.anvl.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 
 /**
  * @author DELL
@@ -16,13 +14,10 @@ import lombok.RequiredArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@RequiredArgsConstructor
 public class Product {
 
-	@NonNull
+	private long id;
 	private String title;
-	@NonNull
-	private Long id;
 	private String type;
 	private String description;
 	private String filename;
@@ -30,5 +25,11 @@ public class Product {
 	private String width;
 	private String price;
 	private String rating;
+
+	public Product(String type, long id) {
+		super();
+		this.type = type;
+		this.id = id;
+	}
 
 }
